f9ac408c85bdb6e75d79a971accebc988af4df14
nicolsc <nicolas.lesconnec@gmail.com>
2015-05-26 17:15:44 +0200

    requestLogger - remove debugs

--- middlewares/requestLogger.js
+++ middlewares/requestLogger.js
@@ -1,12 +1,10 @@
 'use strict';
-const debug = require('debug')('sigfox-demo-TD1204:request-logger');
+const debug = require('debug')('sigfox-connectedconf:request-logger');
 const db = require('../modules/db');
 module.exports = function(req, res, next){
   db.insertOne('request_logs', {date:new Date().toISOString(), method:req.method, path:req.path, data:req.body || req.query})
   .then(function(obj){
     debug('Request log OK');
-    debug(req.body)
-    debug(obj);
     next();
   })
   .catch(function(err){
